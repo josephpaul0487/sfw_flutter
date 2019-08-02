c247dcc914f099fe444da09e32e803604a562c88
josephpaul0487 <josephpaul0487@gmail.com>
2019-08-02 15:10:05 +0530

    no message

--- sfw_generator/lib/src/generator.dart
+++ sfw_generator/lib/src/generator.dart
@@ -519,7 +519,7 @@ class DbGenerator extends Generator {
 
   void loadAssets(StringBuffer s,BuildStep buildStep) async {
     s.writeln("String ddddd='${buildStep.inputId.package} ${buildStep.inputId.path}';");
-    String str= await buildStep.readAsString(AssetId("sfw_generator", "assets/test.txt"));
+    String str= await buildStep.readAsString(AssetId("sfw_generator", "lib/src/assets/test.txt"));
     s.writeln("String ssss='$str';");
 //    DefaultAssetBundle bundle=DefaultAssetBundle.of(context);
 //    s.writeln(await rootBundle.loadString("assets/sfw_html.dart"));
