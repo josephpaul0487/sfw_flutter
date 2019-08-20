2d97aa4d689c563f5bf65842359782202b5437be
Joseph paul <josephpaul0487@gmail.com>
2019-08-20 09:49:14 +0530

    no message

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # sfw_flutter
 Simple database generator (private use only)
+
+
--- sfw_imports/lib/sfw_imports.dart
+++ sfw_imports/lib/sfw_imports.dart
@@ -2,7 +2,6 @@ library sfw_imports;
 export 'src/app_interfaces.dart';
 export 'src/db.dart';
 export 'src/helper_models.dart';
-export 'src/sfw_styles.dart';
 export 'src/validator.dart';
 export 'src/web.dart';
 export 'src/others.dart';
