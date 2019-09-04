8efaadf62659626aef63a87454b90136468eece5
Joseph paul <josephpaul0487@gmail.com>
2019-09-04 12:40:36 +0530

    no message

--- sfw_generator/lib/src/assets/sfw_ui.d
+++ sfw_generator/lib/src/assets/sfw_ui.d
@@ -1,7 +1,39 @@
 class SfwUi {
-  static void showToast(
-      String msg,
-      ) {
+  static bool hasPermission(PermissionGroup permission,
+      [String toastMessage]) async {
+    PermissionStatus permission = await PermissionHandler()
+        .checkPermissionStatus(permission);
+    if (permission == null || permission != PermissionStatus.granted) {
+      Map<PermissionGroup,
+          PermissionStatus> permissions = await PermissionHandler()
+          .requestPermissions([permission]);
+      if (permissions == null || permissions.length == 0 ||
+          !permissions.containsKey(permission) ||
+          permissions[permission] != PermissionStatus.granted) {
+        if (toastMessage != null && toastMessage.isNotEmpty)
+          WidgetHelper.showToast(toastMessage);
+        return false;
+      }
+    }
+    return true;
+  }
+
+  static bool hasPermissions(List<PermissionGroup> permissions,
+      [List<String> toastMessages]) async {
+    bool isToastEmpty=toastMessages==null || toastMessages.isEmpty;
+    if(!isToastEmpty && permissions.length!=toastMessages.length) {
+      throw Exception("Either toastMessages is empty or toastMessages.length == permissions.length");
+    }
+
+    for(int i=0;i<permissions.length;++i) {
+      if(!await hasPermission(permissions[i],isToastEmpty?null:toastMessages[i])) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  static void showToast(String msg,) {
     try {
       Fluttertoast.showToast(
           msg: msg,
@@ -11,7 +43,6 @@ class SfwUi {
           backgroundColor: Colors.black,
           textColor: Colors.white,
           fontSize: SfwHelper.setSp(30.0));
-    } catch(e){}
-
+    } catch (e) {}
   }
 }
