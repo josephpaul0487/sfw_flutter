b504733595979ecde3c8313c13e8a8a8c81ed296
Joseph paul <josephpaul0487@gmail.com>
2019-09-07 12:06:30 +0530

    Read me.  and Notifiers

--- README.md
+++ README.md
@@ -1,9 +1,230 @@
 # sfw_flutter
 Simple database generator (private use only)
 
-Import following libraries
+## Codes to generate files
 
-intl:
-dio:
-sqflite:
-permission_handler:
+### Step 1:  flutter packages pub run build_runner clean
+### Step 2:  flutter packages pub run build_runner build      OR
+###          flutter packages pub run build_runner build --delete-conflicting-outputs
+
+## IMPORT
+*  Import following libraries
+
+        dependencies:
+          sfw_generator:
+            git:
+              url: git://github.com/josephpaul0487/sfw_flutter.git
+              path: sfw_generator
+          flutter:
+            sdk: flutter
+
+
+          sqflite: ^1.1.6+4
+          permission_handler: ^3.2.2
+          fluttertoast: ^3.1.2
+          dio: ^2.1.16
+          intl: ^0.16.0
+
+        dev_dependencies:
+          build_runner: ^1.6.7
+
+
+
+
+
+## Database Config
+* Annotate a class using @SfwDbConfig("dbname",6,version:8)
+
+    name, totalDartFileCount, version
+
+    The newly created filename will be thisClassName.sfw.dart
+
+
+    * should create following functions in this class
+    * You can use database object in these functions
+    static onOpen(db) {}
+    static onCreate(db, version){}
+    static onUpgrade(db, version, newVersion){}
+    static onDowngrade(db, version, newVersion){}
+
+    * You can use SfwDbQuery to generate query functions
+
+    import 'package:sfw_imports/sfw_imports.dart' show SfwDbConfig,SfwDbQuery;
+    @SfwDbConfig("quizup",6,version:3)
+    abstract class DB {
+        static onOpen(db) {}
+        static onCreate(db, version){}
+        static onUpgrade(db, version, newVersion){}
+        static onDowngrade(db, version, newVersion){}
+
+        @SfwDbQuery(UserModel,false,'tbl_user',limit: 1,orderBy: "updated_at")
+         getUser();
+
+    }
+
+## Database model classes
+
+    #### Create model classes and annotate it with SfwEntity
+
+    * The model class should annotate with @SfwEntity
+    * Don't use getter or setter for Entity class fields
+    * The constructor should be a named constructor
+    * Contructor should have all fields as parameter
+    * Should annotate a int field with SfwDbPrimary  -  SfwDbPrimary(true) to set auto increment in primary field
+    * You can set database column name with  @SfwDbField("name")
+        * You can pass null or "" to set field name as database column name
+        * Field name will use as column name if you are not annotate the field with SfwDbField
+    * Use @SfwDbExclude() to exclude a field from the database table
+        * You can specify which tables should exclude - @SfwDbExclude(tables: ["memebrs"])
+
+    * You can use multiple tables in a model class -  See example
+    * Inheritance not supported
+    * List type and Custom classes should annoate with @SfwDbField and should provide genericType: and isAnEntity: parameters
+        * genericType : List's generic type or custom class type
+
+
+    #### Not supporting foreign keys and other complicated features
+
+
+
+    @SfwEntity(["tbl_user","members"])
+    class UserModel {
+      @SfwDbPrimary(false)
+      int id;
+      @SfwDbField(null)
+      String email;
+      @SfwDbField("")
+      String name;
+      @SfwDbExclude()
+      String role;
+      @SfwDbExclude()
+      String countryCode;
+      String mobile;
+      String token;
+      String image;
+      @SfwDbField("created_at")
+      String createdAt;
+      @SfwDbField("updated_at")
+      String updatedAt;
+      @SfwDbField("pages",genericType: PageModel,isAnEntity: true)
+      List<PageModel> pages;
+
+      bool keepSignedIn;
+
+      @SfwDbField("workAddress",genericType: String,isAnEntity:false)
+      List<String> address;
+
+      UserModel(
+          {this.id,
+            this.email,
+            this.name,
+            this.role,
+            this.countryCode,
+            this.mobile,
+            this.token,
+            this.image,
+            this.createdAt,
+            this.updatedAt,
+            this.keepSignedIn,
+          this.pages,this.address})
+
+    }
+
+
+## SfwHelper
+
+    #### Copied code from  https://github.com/OpenFlutter/flutter_screenutil - flutter_screenutil
+
+    * should initialize this with SfwHelper.initialize(context)
+    * adapting screen and font size. Let your UI display a reasonable layout on different screen sizes!
+    * have some date conversion helpers
+    * hide keyboard function
+
+
+    Container(
+      width: SfwHelper.setWidth(300),
+      height: SfwHelper.setHeight(300),
+      child: Text(
+        "text",
+        style: TextStyle(fontSize: SfwHelper.setSp(20)),
+      ),
+    );
+
+
+
+
+## SfwAnim
+
+    * for navigation using CupertinoPageRoute
+
+    SfwAnim.navigate(context, "page name", Splash(),type: NavigationType.REMOVE_NAMED_UNTIL,arguments:[],useAsync: true);
+
+    * set useAsync parameter true if you want to wait for result
+
+    * Have fadeAnimation  and scaleAnimation  widget
+
+
+
+## SfwUi
+
+    * Have some ui related functions
+    * hasPermission to check the permission
+    * showToast for toast messages
+
+
+## SfwHtmlParser   and   HtmlParser
+
+    * Html to text parser  -> copied from a library
+
+
+## SfwState
+
+    * Replace extends State to SfwState
+    * You can call setState with null parameter
+    * You can use toastMessage function to show a message
+    * isEmpty function will validate a string is empty or not and showing a message
+
+## SfwNotifier
+
+    * A notifier like java interface
+    * You can use this in a class
+    * You can use a set of keys
+    * Class should implement SfwNotifierListener
+    * override   void onSfwNotifierCalled(String key, dynamic data)
+    * SfwNotifier.addListener(this,{keys})  to add a listener
+    * SfwNotifier.removeListener(this) to remove a listener  ->  should call on dispose or equalant method
+
+    ##### To notify all listener
+
+    * SfwNotifier.notify(key,data); //This will trigger all listeners's (registered with this key) onSfwNotifierCalled function
+
+## SfwNotifierForSingleKey
+
+    * Same as SfwNotifier  but can only use one key for a listener
+    * Use SfwNotifier for multiple keys or Add multiples keys with SfwNotifierForSingleKey.addListener  one by one
+
+## SfwNotifierSingleKeyWidget
+
+    * You can use this widget to update a single widget on data change without updating all widgets
+    * This will use SfwNotifierForSingleKey.
+    * Use SfwNotifierForSingleKey.notify(key,data); to notify this widget
+    * Use builder function to build your widget
+
+
+## SfwNotifierMultiKeyWidget
+
+    * Same as SfwNotifierSingleKeyWidget
+    * Can use multiple keys
+    * Use SfwNotifier.notify(key,data); to notify this widget
+
+
+## FadeAnimWidget , ScaleAnimWidget , RotateAnimWidget , DrawerAnimWidget , CollapseAnimWidget
+
+    * Animated widgets
+
+
+
+
+
+
+
--- sfw_generator/lib/src/assets/app_helper.d
+++ sfw_generator/lib/src/assets/app_helper.d
@@ -1,41 +1,189 @@
+typedef SfwNotifierBuilder=Widget Function(String key, dynamic data);
 
 abstract class SfwNotifierListener {
-  void onSfwNotifierCalled(String key,dynamic data);
+  void onSfwNotifierCalled(String key, dynamic data);
 }
 
 class SfwNotifier {
-  Map<SfwNotifierListener,Set<String>> _listeners={};
-  static SfwNotifier _notifier=SfwNotifier();
+  final Map<SfwNotifierListener, Set<String>> _listeners = {};
+  static SfwNotifier _notifier = SfwNotifier();
+
   //Map<SfwNotifierListener,Set<String>> get listener=>_listeners;
 
-  static addListener(SfwNotifierListener listener,Set<String> keys) {
-    if(listener==null)
+  static addListener(SfwNotifierListener listener, Set<String> keys) {
+    if (listener == null)
       return;
-    Set<String> list=_notifier._listeners[listener];
-    if(list==null) {
-      list={};
+    Set<String> list = _notifier._listeners[listener];
+    if (list == null) {
+      list = {};
     }
-    if(keys!=null)
+    if (keys != null)
       list.addAll(keys);
-    _notifier._listeners[listener]=list;
+    _notifier._listeners[listener] = list;
   }
 
   static removeListener(SfwNotifierListener listener) {
-    if(listener==null)
+    if (listener == null)
       return;
     _notifier._listeners.remove(listener);
   }
 
-  static notify(String key,dynamic value) {
-    _notifier._listeners.forEach((listener,_keys){
-      if(_keys.isEmpty || _keys.contains(key)) {
+  static notify(String key, dynamic value) {
+    _notifier._listeners.forEach((listener, _keys) {
+      if (listener != null && _keys.isEmpty || _keys.contains(key)) {
         listener.onSfwNotifierCalled(key, value);
       }
     });
   }
+}
+
+class SfwNotifierForSingleKey {
+  final Map<String, Set<SfwNotifierListener>> _listeners = {};
+  static SfwNotifierForSingleKey _notifier = SfwNotifierForSingleKey();
+
+  static addListener(SfwNotifierListener listener, String key) {
+    if (listener == null || key == null || key
+        .trim()
+        .isEmpty)
+      return;
+    Set<SfwNotifierListener> list = _notifier._listeners[key];
+    if (list == null) {
+      list = {};
+    }
+    list.add(listener);
+    _notifier._listeners[key] = list;
+  }
+
+  static removeListener(SfwNotifierListener listener, String key) {
+    if (listener == null || key == null || key
+        .trim()
+        .isEmpty)
+      return;
+    Set<SfwNotifierListener> list = _notifier._listeners[key];
+    if (list == null) {
+      return;
+    }
+    list.remove(listener);
+    if (list.isEmpty) {
+      _notifier._listeners.remove(key);
+    } else {
+      _notifier._listeners[key] = list;
+    }
+  }
+
+  static notify(String key, dynamic value) {
+    if (key == null || key
+        .trim()
+        .isEmpty)
+      return
+        _notifier._listeners[key].forEach((listener) {
+          if (listener != null)
+            listener.onSfwNotifierCalled(key, value);
+        });
+  }
+}
+
+
+class SfwNotifierSingleKeyWidget extends StatefulWidget {
+  final SfwNotifierBuilder builder;
+  final String notifierKey;
+  final dynamic initialData;
+
+  const SfwNotifierSingleKeyWidget(
+      {Key key, @required this.builder, @required this.notifierKey, this.initialData})
+      : assert(builder != null),
+        assert(notifierKey != null && notifierKey != ""),
+        super(key: key);
+
+  @override
+  _SfwNotifierWidgetState createState() => _SfwNotifierWidgetState();
+}
+
+class _SfwNotifierWidgetState extends State<SfwNotifierSingleKeyWidget>
+    implements SfwNotifierListener {
+
+  dynamic data;
+
+  @override
+  void initState() {
+    data = widget.initialData;
+    SfwNotifierForSingleKey.addListener(this, widget.notifierKey);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return widget.builder(widget.notifierKey, data);
+  }
+
+  @override
+  void onSfwNotifierCalled(String key, data) {
+    setState(() {
+      this.data = data;
+    });
+  }
+
+  @override
+  void dispose() {
+    SfwNotifierForSingleKey.removeListener(this, widget.notifierKey);
+    super.dispose();
+  }
 
 
 }
+
+class SfwNotifierMultiKeyWidget extends StatefulWidget {
+  final SfwNotifierBuilder builder;
+  final Set<String> notifierKeys;
+  final dynamic initialData;
+  final String initialKey;
+
+  const SfwNotifierMultiKeyWidget(
+      {Key key, @required this.builder, @required this.notifierKeys, this.initialData, this.initialKey})
+      : assert(builder != null),
+        assert(notifierKeys != null),
+        super(key: key);
+
+  @override
+  _SfwNotifierMultiKeyWidgetState createState() =>
+      _SfwNotifierMultiKeyWidgetState();
+}
+
+class _SfwNotifierMultiKeyWidgetState extends State<SfwNotifierMultiKeyWidget>
+    implements SfwNotifierListener {
+
+  dynamic data;
+  String key;
+
+  @override
+  void initState() {
+    data = widget.initialData;
+    SfwNotifier.addListener(this, widget.notifierKeys);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return widget.builder(key, data);
+  }
+
+  @override
+  void onSfwNotifierCalled(String key, data) {
+    setState(() {
+      this.key = key;
+      this.data = data;
+    });
+  }
+
+  @override
+  void dispose() {
+    SfwNotifier.removeListener(this);
+    super.dispose();
+  }
+
+
+}
+
 
 class SfwHelper {
   static ScreenUtil util;
@@ -43,12 +191,12 @@ class SfwHelper {
   static double screenWidthPx;
   static double screenHeight;
   static double screenHeightPx;
-  static const String yyyy_MM_dd_HH_mm_ss_S_Z="yyyy-MM-dd HH:mm:ss.SZ";
-  static const String MM_dd_yyyy_KK_mm_ss__a="MM-dd-yyyy KK:mm:ss a";
-  static const String MM_dd_yyyy_KK_mm_ss_a="MM-dd-yyyy KK:mm:ssa";
+  static const String yyyy_MM_dd_HH_mm_ss_S_Z = "yyyy-MM-dd HH:mm:ss.SZ";
+  static const String MM_dd_yyyy_KK_mm_ss__a = "MM-dd-yyyy KK:mm:ss a";
+  static const String MM_dd_yyyy_KK_mm_ss_a = "MM-dd-yyyy KK:mm:ssa";
 
   static initialize(BuildContext context) {
-    if (util == null || util.scaleWidth<1) {
+    if (util == null || util.scaleWidth < 1) {
       util = ScreenUtil();
       util.init(context);
       screenHeight = ScreenUtil.screenHeightDp;
@@ -86,15 +234,15 @@ class SfwHelper {
     return num;
   }
 
-  static String convertDate(String fromFormat,String toFormat,String date) {
+  static String convertDate(String fromFormat, String toFormat, String date) {
     try {
-      return DateFormat(toFormat).format( DateFormat(fromFormat).parse(date));
+      return DateFormat(toFormat).format(DateFormat(fromFormat).parse(date));
     } on FormatException {
       return "";
     }
   }
 
-  static String convertDateTime( String toFormat, DateTime date) {
+  static String convertDateTime(String toFormat, DateTime date) {
     try {
       return DateFormat(toFormat).format(date);
     } on FormatException {
@@ -102,114 +250,125 @@ class SfwHelper {
     }
   }
 
-  static String convertWebToLocalDate(String toFormat,String date,{String fromFormat="yyyy-MM-dd HH:mm:ss"}) {
+  static String convertWebToLocalDate(String toFormat, String date,
+      {String fromFormat = "yyyy-MM-dd HH:mm:ss"}) {
     try {
-      return DateFormat(toFormat).format( DateFormat(fromFormat).parse(date));
+      return DateFormat(toFormat).format(DateFormat(fromFormat).parse(date));
     } on FormatException {
       return "";
     }
   }
 
-  static String convertUtcToLocalDate(String fromFormat,String toFormat,String date) {
+  static String convertUtcToLocalDate(String fromFormat, String toFormat,
+      String date) {
     try {
-      return DateFormat(toFormat).format( DateFormat(fromFormat).parse(date));
+      return DateFormat(toFormat).format(DateFormat(fromFormat).parse(date));
     } on FormatException {
       return "";
     }
   }
 
-  static String convertLocalToUtcDate(String fromFormat,String toFormat,String date) {
+  static String convertLocalToUtcDate(String fromFormat, String toFormat,
+      String date) {
     try {
-      return DateFormat(toFormat).format( DateFormat(fromFormat).parse(date));
-    } catch(e)  {
+      return DateFormat(toFormat).format(DateFormat(fromFormat).parse(date));
+    } catch (e) {
       print(e);
       return "";
     }
   }
 
-  static String convertLocalToWebDate(String fromFormat,String date,{String toFormat="yyyy-MM-dd HH:mm:ss"}) {
-    return convertLocalToUtcDate(fromFormat,toFormat,date);
+  static String convertLocalToWebDate(String fromFormat, String date,
+      {String toFormat = "yyyy-MM-dd HH:mm:ss"}) {
+    return convertLocalToUtcDate(fromFormat, toFormat, date);
   }
 
-  static bool isDateAfter(String format,String date,String dateToCompare) {
+  static bool isDateAfter(String format, String date, String dateToCompare) {
     try {
-      return DateFormat(format).parse(date).isAfter( DateFormat(format).parse(dateToCompare));
-    } catch(e) {
+      return DateFormat(format).parse(date).isAfter(
+          DateFormat(format).parse(dateToCompare));
+    } catch (e) {
       return false;
     }
   }
 
-  static bool isDateBefore(String format,String date,String dateToCompare) {
+  static bool isDateBefore(String format, String date, String dateToCompare) {
     try {
-      return DateFormat(format).parse(date).isBefore( DateFormat(format).parse(dateToCompare));
-    } catch(e) {
+      return DateFormat(format).parse(date).isBefore(
+          DateFormat(format).parse(dateToCompare));
+    } catch (e) {
       return false;
     }
   }
 
-  static bool isEqual(String format,String date,String dateToCompare) {
+  static bool isEqual(String format, String date, String dateToCompare) {
     try {
-      return DateFormat(format).parse(date).isAtSameMomentAs( DateFormat(format).parse(dateToCompare));
-    } catch(e) {
+      return DateFormat(format).parse(date).isAtSameMomentAs(
+          DateFormat(format).parse(dateToCompare));
+    } catch (e) {
       return false;
     }
   }
 
-  static bool isDateAfterDateTime(String format,String date,DateTime dateToCompare) {
+  static bool isDateAfterDateTime(String format, String date,
+      DateTime dateToCompare) {
     try {
-      return DateFormat(format).parse(date).isAfter( dateToCompare);
-    } catch(e) {
+      return DateFormat(format).parse(date).isAfter(dateToCompare);
+    } catch (e) {
       return false;
     }
   }
 
-  static bool isDateBeforeDateTime(String format,String date,DateTime dateToCompare) {
+  static bool isDateBeforeDateTime(String format, String date,
+      DateTime dateToCompare) {
     try {
-      return DateFormat(format).parse(date).isBefore( dateToCompare);
-    } catch(e) {
+      return DateFormat(format).parse(date).isBefore(dateToCompare);
+    } catch (e) {
       return false;
     }
   }
 
-  static bool isEqualDateTime(String format,String date,DateTime dateToCompare) {
+  static bool isEqualDateTime(String format, String date,
+      DateTime dateToCompare) {
     try {
-      return DateFormat(format).parse(date).isAtSameMomentAs( dateToCompare);
-    } catch(e) {
+      return DateFormat(format).parse(date).isAtSameMomentAs(dateToCompare);
+    } catch (e) {
       return false;
     }
   }
 
-  static DateTime parse(String format,String date,{DateTime defDateTime,bool isUtc=false}) {
+  static DateTime parse(String format, String date,
+      {DateTime defDateTime, bool isUtc = false}) {
     try {
       return DateFormat(format).parse(date);
-    } catch(e) {
+    } catch (e) {
       return defDateTime ?? null;
     }
   }
 
 
-
-
   static void hideKeyboard(BuildContext ctx) {
     try {
       FocusScope.of(ctx).requestFocus(FocusNode());
-     // SystemChannels.textInput.invokeMethod('TextInput.hide');
-    } catch(e) {}
-
+      // SystemChannels.textInput.invokeMethod('TextInput.hide');
+    } catch (e) {}
   }
 
   static String extension(String fileUrl) {
-    return fileUrl==null || !fileUrl.contains(".")?"":fileUrl.substring(fileUrl.indexOf(".")+1);;
+    return fileUrl == null || !fileUrl.contains(".") ? "" : fileUrl.substring(
+        fileUrl.indexOf(".") + 1);;
   }
 
   static bool isDocument(fileUrl) {
-    String ext=extension(fileUrl).toLowerCase();
-    return ext!="" && (ext=='pdf' || ext=='doc' || ext=='txt' || ext=='docx' || ext=='ppt' || ext=='pptx' || ext=='xls' || ext=='xlsx');
+    String ext = extension(fileUrl).toLowerCase();
+    return ext != "" &&
+        (ext == 'pdf' || ext == 'doc' || ext == 'txt' || ext == 'docx' ||
+            ext == 'ppt' || ext == 'pptx' || ext == 'xls' || ext == 'xlsx');
   }
 
   static bool isImage(fileUrl) {
-    String ext=extension(fileUrl).toLowerCase();
-    return ext!="" && (ext=='jpg' || ext=='jpeg' || ext=='png');
+    String ext = extension(fileUrl).toLowerCase();
+    return ext != "" && (ext == 'jpg' || ext == 'jpeg' || ext == 'png');
   }
 
 
